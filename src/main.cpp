2ed380b257b74fba37a2faeedaab685bb503b7d4
Rafael Schreiber <rafael@backend.works>
2021-01-10 18:41:01 +0100

    program proof of concept done

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,15 @@
  */
 
 #include <iostream>
-#include <thread>
+#include <string>
+#include <unistd.h>
+#include <sstream>
+#include <csignal>
 
+#include "tfile.h"
+#include "CLI11.hpp"
+
+// use __float128 if compiler and machine support it for more precision
 #ifdef USE_FLOAT128
     typedef __float128  long_double_t;
 #else
@@ -15,6 +22,18 @@
 #endif
 
 using namespace std;
+
+template <typename T>
+string to_string_w_precision(const T a_value, const int n = 128){
+    ostringstream out;
+    out.precision(n);
+    out << fixed << a_value;
+    return out.str();
+}
+
+uint64_t n; // global variable for determine iterations
+string result_file_path;
+size_t finished_processes{0};
 
 long_double_t calc_part_leibniz(uint64_t n, bool is_positive){
     // is_positive determines if term is negative or positive 
@@ -34,14 +53,76 @@ long_double_t calc_part_leibniz(uint64_t n, bool is_positive){
     return part_result;
 }
 
+void calc_positive(int signal){
+    string result_string{to_string_w_precision(calc_part_leibniz(n, true))};
+    string storepath{result_file_path + "part_pos.txt"};
+    tfile::write(storepath.c_str(), result_string);
+}
+
+void calc_negative(int signal){
+    string result_string{to_string_w_precision(calc_part_leibniz(n, false))};
+    string storepath{result_file_path + "part_neg.txt"};
+    tfile::write(storepath.c_str(), result_string);
+}
+
+inline void increment_finished_processes(int signal){
+    finished_processes++;
+}
+
+void calc_pi(){
+    long_double_t pos, neg, pi;
+    string pos_str, neg_str;
+    string storepath_pos{result_file_path + "part_pos.txt"};
+    string storepath_neg{result_file_path + "part_neg.txt"};
+    pos_str = tfile::read(storepath_pos.c_str());
+    neg_str = tfile::read(storepath_neg.c_str());
+    pos = stold(pos_str);
+    neg = stold(neg_str);
+    pi = (pos + neg) * 4;
+    cout << to_string_w_precision(pi) << endl;
+}
 
 
 // int main(int argc, char** argv) {
 int main(){
-    uint64_t n = 1'000'000'000;
-    long_double_t pos{calc_part_leibniz(n, true)};
-    long_double_t neg{calc_part_leibniz(n, false)};
-    cout.precision(20);
-    long_double_t pi = (pos + neg) * 4;
-    cout << pi << endl;
+    n = 500'000'000;
+    result_file_path = "/Users/rafael/Desktop/";
+    int child_process_1;
+    int child_process_2;
+    child_process_1 = fork();
+    if (child_process_1 == 0){
+        printf("Child Process 1:\npid :%d\nppid:%d\n", getpid(), getppid());
+        signal(SIGHUP, calc_positive);
+        pause();
+        cout << "done_pos" << endl;
+        kill(getppid(), SIGHUP);
+        pause();
+    }
+    if (child_process_1 > 0){
+        child_process_2 = fork();
+        if(child_process_2 > 0){
+            printf("\nParent Process:\npid:%d\nppid :%d\n", getpid(), getppid());
+            usleep(1'000'000);
+            kill(child_process_1, SIGHUP);
+            kill(child_process_2, SIGHUP);
+            signal(SIGHUP, increment_finished_processes);
+            while (finished_processes < 2){ // wait for subprocesses to finish
+                usleep(1'000'000);
+            }
+            kill(child_process_1, SIGKILL);
+            kill(child_process_2, SIGKILL);
+            waitpid(child_process_1, nullptr, 0);
+            waitpid(child_process_2, nullptr, 0);
+            cout << "was geht jungs!" << endl;
+            calc_pi();
+        }
+        else if(child_process_2 == 0){
+            printf("Child Process 2:\npid :%d\nppid:%d\n", getpid(), getppid());
+            signal(SIGHUP, calc_negative);
+            pause();
+            cout << "done_neg" << endl;
+            kill(getppid(), SIGHUP);
+            pause();
+        }
+    }
 }
